11d1ec92038db8848063d1b343487f58e5ef1e64
akshay <akshay.py@gmail.com>
2018-04-04 19:17:04 +0530

    register user commit

--- flightreservation/src/main/java/com/akshay/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/akshay/flightreservation/controllers/UserController.java
@@ -1,12 +1,27 @@
 package com.akshay.flightreservation.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.akshay.flightreservation.entities.User;
+import com.akshay.flightreservation.repos.UserRepository;
 
 @Controller
 public class UserController {
+	@Autowired
+	UserRepository userRepo;
+
 	@RequestMapping("/showReg")
 	public String showRegistrationPage() {
 		return "login/registerUser";
+	}
+
+	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
+	public String register(@ModelAttribute("user") User user) {
+		userRepo.save(user);
+		return "login/login";
 	}
 }
