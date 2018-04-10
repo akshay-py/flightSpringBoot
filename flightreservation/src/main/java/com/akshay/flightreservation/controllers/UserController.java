d4f7701a304c32f36dfab0a80d5d91da95a882c6
akshay <akshay.py@gmail.com>
2018-04-10 22:54:53 +0530

    flights

--- flightreservation/src/main/java/com/akshay/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/akshay/flightreservation/controllers/UserController.java
@@ -2,9 +2,11 @@ package com.akshay.flightreservation.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.akshay.flightreservation.entities.User;
 import com.akshay.flightreservation.repos.UserRepository;
@@ -19,9 +21,25 @@ public class UserController {
 		return "login/registerUser";
 	}
 
+	@RequestMapping("/showLogin")
+	public String showLoginPage() {
+		return "login/login";
+	}
+
 	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
 	public String register(@ModelAttribute("user") User user) {
 		userRepo.save(user);
+		return "login/login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestParam("email") String email, @RequestParam("password") String password, ModelMap modelMap) {
+		User user = userRepo.findByEmail(email);
+		if (user != null && user.getPassword().equals(password)) {
+			return "findFlights";
+		} else {
+			modelMap.addAttribute("msg", "Invalid user name or password. Please try again.");
+		}
 		return "login/login";
 	}
 }
